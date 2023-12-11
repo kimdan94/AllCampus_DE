ff894f52a6b3da85189fe403503568c3fe06094f
Anna Cho <rema1ny0un9@gmail.com>
2023-12-11 19:28:39 +0900

    CheckDuplicatedAction up

--- AllCampus/src/main/java/kr/member/action/CheckDuplicatedAction.java
+++ AllCampus/src/main/java/kr/member/action/CheckDuplicatedAction.java
@@ -16,6 +16,7 @@ public class CheckDuplicatedAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
 		//전송된 데이터 인코딩 처리
 		request.setCharacterEncoding("utf-8");
 		
