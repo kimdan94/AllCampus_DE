057f82c435a4ae6ef90a3acad1ccfea6f6cb9e91
Anna Cho <rema1ny0un9@gmail.com>
2023-12-11 19:30:55 +0900

    CheckIdAction up

---
+++ AllCampus/src/main/java/kr/member/action/CheckIdAction.java
@@ -0,0 +1,30 @@
+package kr.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+import kr.member.dao.MemberDAO;
+
+public class CheckIdAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//전송된 데이터 인코딩 처리
+		request.setCharacterEncoding("utf-8");
+		
+		String email = request.getParameter("email");
+		
+		MemberDAO dao = MemberDAO.getinstance();
+		String user_id = dao.checkId(email);
+		if(user_id != null) {
+			request.setAttribute("user_email", email);
+			request.setAttribute("user_id", user_id);
+		}
+		
+		request.setAttribute("user_email", email);
+		
+		return "/WEB-INF/views/member/checkId.jsp";
+	}
+
+}
