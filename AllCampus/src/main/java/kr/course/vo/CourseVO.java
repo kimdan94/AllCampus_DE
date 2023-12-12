04475d8cbfd9d1385a27d3adb43a449d2831a399
Nahyun-K <rnjsskgus990313@gmail.com>
2023-12-12 12:09:16 +0900

    course 데이터 필터링 기능 추가

---
+++ AllCampus/src/main/java/kr/course/vo/CourseVO.java
@@ -0,0 +1,104 @@
+package kr.course.vo;
+
+public class CourseVO {
+	private int course_num;
+	private String course_name;
+	private String course_prof;
+	private int course_year;
+	private int course_semester;
+	private String course_subject;
+	private int course_day;
+	private String course_start_time;
+	private String course_end_time;
+	private String course_category;
+	private int course_credit;
+	private String course_classroom;
+	private String course_code;
+	private int univ_num;
+	
+	public int getCourse_num() {
+		return course_num;
+	}
+	public void setCourse_num(int course_num) {
+		this.course_num = course_num;
+	}
+	public String getCourse_name() {
+		return course_name;
+	}
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+	public String getCourse_prof() {
+		return course_prof;
+	}
+	public void setCourse_prof(String course_prof) {
+		this.course_prof = course_prof;
+	}
+	public int getCourse_year() {
+		return course_year;
+	}
+	public void setCourse_year(int course_year) {
+		this.course_year = course_year;
+	}
+	public int getCourse_semester() {
+		return course_semester;
+	}
+	public void setCourse_semester(int course_semester) {
+		this.course_semester = course_semester;
+	}
+	public String getCourse_subject() {
+		return course_subject;
+	}
+	public void setCourse_subject(String course_subject) {
+		this.course_subject = course_subject;
+	}
+	public int getCourse_day() {
+		return course_day;
+	}
+	public void setCourse_day(int course_day) {
+		this.course_day = course_day;
+	}
+	public String getCourse_start_time() {
+		return course_start_time;
+	}
+	public void setCourse_start_time(String course_start_time) {
+		this.course_start_time = course_start_time;
+	}
+	public String getCourse_end_time() {
+		return course_end_time;
+	}
+	public void setCourse_end_time(String course_end_time) {
+		this.course_end_time = course_end_time;
+	}
+	public String getCourse_category() {
+		return course_category;
+	}
+	public void setCourse_category(String course_category) {
+		this.course_category = course_category;
+	}
+	public int getCourse_credit() {
+		return course_credit;
+	}
+	public void setCourse_credit(int course_credit) {
+		this.course_credit = course_credit;
+	}
+	public String getCourse_classroom() {
+		return course_classroom;
+	}
+	public void setCourse_classroom(String course_classroom) {
+		this.course_classroom = course_classroom;
+	}
+	public String getCourse_code() {
+		return course_code;
+	}
+	public void setCourse_code(String course_code) {
+		this.course_code = course_code;
+	}
+	public int getUniv_num() {
+		return univ_num;
+	}
+	public void setUniv_num(int univ_num) {
+		this.univ_num = univ_num;
+	}
+	
+}
